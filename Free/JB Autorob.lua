a12761b443b971a6123acd51c93bd0d5cba605b5
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-01-20 21:44:47 +0000

    Update JB Autorob.lua

--- Free/JB Autorob.lua
+++ Free/JB Autorob.lua
@@ -166,6 +166,7 @@ function FarTP(Cframe)
 	if Car ~= nil then
 		Car.CFrame = Root.CFrame * CFrame.new(-5, 0, -2)
 		Car.Anchored = true
+		wait(1)
 		repeat
 			VIM:SendKeyEvent(true, Enum.KeyCode.E, false, game)
 			wait(0.2)

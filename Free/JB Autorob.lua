2c3d8464a8f774394530f0074fb790aecbffa5bf
SykoticCataclysm <48451332+SykoticCataclysm@users.noreply.github.com>
2020-01-20 20:08:36 +0000

    Update JB Autorob.lua

--- Free/JB Autorob.lua
+++ Free/JB Autorob.lua
@@ -75,12 +75,16 @@ Airdrop.TextXAlignment = Enum.TextXAlignment.Right
 	
 -- Setup --
 
+local VIM = game:GetService("VirtualInputManager")
+
 local Plr = game:GetService("Players").LocalPlayer
 local Char = Plr.Character
 local Root = Char:WaitForChild("HumanoidRootPart")
+local Hum = Char:FindFirstChildOfClass("Humanoid")
 
 local Clipped = true
 local Robbing = false
+local RobType = ""
 local Abort = false
 local Teleporting = false
 _G.AutoRobOn = true
@@ -127,36 +131,54 @@ end)
 
 Plr.CharacterAdded:Connect(function(char)
 	Root = char:WaitForChild("HumanoidRootPart")
+	Hum = char:FindFirstChildOfClass("Humanoid")
 	Char = char
 	_G.AutoRobOn = false
 	wait()
 	_G.AutoRobOn = true
 end)
 
-function TP(Cframe, speed)
+function CloseTP(Cframe)
 	Teleporting = true
 	Clipped = false
-	local cf0 = (Cframe - Cframe.p) + Root.Position + Vector3.new(0, 6, 0)
+	local cf0 = (Cframe - Cframe.p) + Root.Position + Vector3.new(0,4,0)
 	local length = Cframe.p - Root.Position
 	workspace.Gravity = 0
-	for i = 0, length.magnitude, speed do
+	for i = 0, length.magnitude, 3 do
 		Root.CFrame = cf0 + length.Unit * i
 		Root.Velocity, Root.RotVelocity = Vector3.new(), Vector3.new()
 		wait()
 	end 
 	Clipped = true
 	workspace.Gravity = 196.2
-	Root.CFrame = Cframe
 	Teleporting = false
 end
 
-function UpTP(Cframe)
+function FarTP(Cframe)
 	Teleporting = true
-	TP(CFrame.new(Root.Position.X, 145, Root.Position.Z), 3)
-	wait(0.2)
-	TP(CFrame.new(Cframe.p.X, 145, Cframe.p.Z), 3.5)
-	wait(0.2)
-	TP(Cframe, 1.5)
+	wait(1)
+	local Car = nil
+	repeat
+		for i, v in pairs(workspace.Vehicles:GetChildren()) do
+			if v.Name == "Camaro" and v:FindFirstChild("Seat") and v.Seat:FindFirstChild("Player") and v.Seat.Player.Value == false and v.Seat:FindFirstChild("PlayerName") and v.Seat.PlayerName.Value == "" then
+				Car = v.Model.Body
+				break
+			end
+		end
+	until Car ~= nil
+	Car.CFrame = Root.CFrame * CFrame.new(-5, 0, -2)
+	wait(0.5)
+	VIM:SendKeyEvent(true, Enum.KeyCode.E, false, game)
+	wait()
+	VIM:SendKeyEvent(false, Enum.KeyCode.E, false, game)
+	wait(1)
+	Car.CFrame = Cframe
+	wait(1)
+	VIM:SendKeyEvent(true, Enum.KeyCode.Space, false, game)
+	wait()
+	VIM:SendKeyEvent(false, Enum.KeyCode.Space, false, game)
+	wait(1)
+	Car.Parent.Parent:Destroy()
 	Teleporting = false
 end
 
@@ -192,24 +214,25 @@ end
 -- Jewelry Store --
 
 function RobJewelry()
-	UpTP(CFrame.new(143.7, 19.1, 1351.5))
+	RobType = "Jewelry"
+	FarTP(CFrame.new(143.7, 19.1, 1351.5))
 	wait(1)
-	TP(CFrame.new(126.3, 19, 1316.9), 3)
+	CloseTP(CFrame.new(126.3, 19, 1316.9))
 	local Jewels = workspace:FindFirstChild("Jewelrys"):GetChildren()[1].Boxes:GetChildren()
 	local Collected = 0
 	for a, b in pairs(Jewels) do
 		repeat wait() until Abort == false
 		if not IsBagFull() and b.Transparency < 0.99 and JewIsOpen == true then
 			if b.Position.X < 120 and b.Position.Z > 1330 then
-				TP(CFrame.new(b.Position + b.CFrame.lookVector * 2.5 + Vector3.new(0, 0, -2.5), b.Position), 3)
+				CloseTP(CFrame.new(b.Position + b.CFrame.lookVector * 2.5 + Vector3.new(0, 0, -2.5), b.Position))
 			elseif b.Position.Z < 1309 and b.Position.Z > 1304 then
-				TP(CFrame.new(b.Position + b.CFrame.lookVector * 2.5 + Vector3.new(0, 0, 2.5), b.Position), 3)
+				CloseTP(CFrame.new(b.Position + b.CFrame.lookVector * 2.5 + Vector3.new(0, 0, 2.5), b.Position))
 			else
-				TP(CFrame.new(b.Position + b.CFrame.lookVector * 2.5, b.Position), 3)
+				CloseTP(CFrame.new(b.Position + b.CFrame.lookVector * 2.5, b.Position))
 			end
 			wait(0.6)
 			for c = 1, 4 do
-				game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.F, false, game)
+				VIM:SendKeyEvent(true, Enum.KeyCode.F, false, game)
 				wait(0.6)
 				repeat wait() until Abort == false
 			end
@@ -218,21 +241,16 @@ function RobJewelry()
 			wait(0.5)
 		end
 	end
-	TP(CFrame.new(154, 18.8, 1270.9), 3)
-	wait(0.2)
-	TP(CFrame.new(141.6, 117.9, 1274.4), 3.5)
-	wait(0.2)
-	TP(CFrame.new(116.3, 117.9, 1307), 3.5)
-	wait(0.2)
-	UpTP(CFrame.new(-229.8, 30, 1602.3), 3)
+	FarTP(CFrame.new(-229.8, 20, 1602.3))
 end
 
 -- Bank --
 
 function RobBank()
-	UpTP(CFrame.new(12.1, 19.1, 790.6))
+	RobType = "Bank"
+	FarTP(CFrame.new(12.1, 19.1, 790.6))
 	wait(1)
-	TP(CFrame.new(25.7, 19.4, 854.3), 3)
+	CloseTP(CFrame.new(25.7, 19.4, 854.3))
 	local Bank = workspace.Banks:GetChildren()[1].Layout:GetChildren()[1]
 	local Door = Bank.Door.Hinge
 	if Bank:FindFirstChild("Lasers") then
@@ -241,16 +259,17 @@ function RobBank()
 		end
 	end
 	wait(1)
-	TP(Bank.TriggerDoor.CFrame * CFrame.new(0, 0, -2), 3)
+	CloseTP(Bank.TriggerDoor.CFrame * CFrame.new(0, 0, -2))
 	wait(0.5)
-	TP(Bank.Money.CFrame, 3)
+	CloseTP(Bank.Money.CFrame)
 	repeat wait() until IsBagFull() == true or Abort == true or BankIsOpen == false
 end
 
 -- Museum --
 
 function RobMuseum()
-	UpTP(CFrame.new(1085.8, 143.8, 1201.7))
+	RobType = "Museum"
+	FarTP(CFrame.new(1085.8, 143.8, 1201.7))
 	local Museum = workspace:FindFirstChild("Museum")
 	for i, v in ipairs(Museum.CaseLasers:GetChildren()) do
 		v:Destroy()
@@ -258,22 +277,24 @@ function RobMuseum()
 	for i, v in ipairs(Museum.Dino:GetChildren()) do
 		if v.Transparency < 0.99 and MuseumBagFull() == false and MuseumIsOpen == true then
 			repeat 
-				TP(CFrame.new(v.Position.X, v.Position.Y + 2, v.Position.Z), 2)
+				CloseTP(CFrame.new(v.Position.X, v.Position.Y + 2, v.Position.Z))
 				wait(0.5)
-				game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.E, false, game)
+				VIM:SendKeyEvent(true, Enum.KeyCode.E, false, game)
 				wait(2)
-				game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.E, false, game)
+				VIM:SendKeyEvent(false, Enum.KeyCode.E, false, game)
 			until v.Transparency > 0.99 or MuseumIsOpen == false or Abort == true
 		end
 	end
+	CloseTP(CFrame.new(1085.8, 143.8, 1201.7))
 	if string.split(MuseumBag.Text, " ")[1] ~= "0" then
-		UpTP(CFrame.new(1638.9, 51.1, -1799.1))
+		FarTP(CFrame.new(1638.9, 51.1, -1799.1))
 	end
 end
 
 -- Airdrop --
 
 function RobAirdrop()
+	RobType = "Airdrop"
 	local Drop = nil
 	local Dist = 999999
 	for i, v in pairs(Airdrops) do
@@ -284,13 +305,13 @@ function RobAirdrop()
 	end
 	if Drop ~= nil then
 		local Loc = Drop.Briefcase.CFrame
-		UpTP(Loc)
+		FarTP(Loc)
 		repeat
-			TP(Loc, 3)
+			CloseTP(Loc)
 			wait(0.5)
-			game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.E, false, game)
+			VIM:SendKeyEvent(true, Enum.KeyCode.E, false, game)
 			wait(7)
-			game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.E, false, game)
+			VIM:SendKeyEvent(false, Enum.KeyCode.E, false, game)
 		until Drop == nil or Drop.Parent == nil or Abort == true
 	end
 end
@@ -324,7 +345,7 @@ spawn(function()
 					RobAirdrop()
 					Robbing = false
 				elseif (Vector3.new(554.5, 20, 1117.4) - Root.Position).magnitude > 15 then
-					UpTP(CFrame.new(554.5, 20, 1117.4), 3.5)
+					FarTP(CFrame.new(554.5, 20, 1117.4), 3.5)
 				end
 			end
 		end
@@ -394,13 +415,13 @@ end)
 -- Cop Detection --
 
 spawn(function()
-	while wait(0.25) do
-		if Teleporting == false then
+	while wait() do
+		if Teleporting == false and Robbing == true and RobType == "Bank" then
 			for i, v in ipairs(game:GetService("Teams").Police:GetPlayers()) do
 				if v.Character ~= nil and v.Character:FindFirstChild("HumanoidRootPart") then
-					if (v.Character.HumanoidRootPart.Position - Root.Position).magnitude < 40 then
+					if (v.Character.HumanoidRootPart.Position - Root.Position).magnitude < 15 then
 						Abort = true
-						UpTP(CFrame.new(554.5, 20, 1117.4))
+						FarTP(CFrame.new(554.5, 20, 1117.4))
 						Abort = false
 					end
 				end
